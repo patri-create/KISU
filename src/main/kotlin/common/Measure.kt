59642f383dbd77fdd07d05843e14d3c496b91cac
patri-create <patriciamkv@gmail.com>
2025-04-12 11:36:30 +0200

    Add readableAutoScale and readable

--- src/main/kotlin/common/Measure.kt
+++ src/main/kotlin/common/Measure.kt
@@ -6,43 +6,38 @@ class Measure<T> private constructor(
     private val magnitude: Double,
     private val prefix: T,
     private val unit: String,
-    private val autoScale: Boolean = true,
-    private val decimalFormat: Boolean = true
 ) where T : Enum<T>, T : Prefix {
 
+    private val readableAutoScale by lazy {
+        (Prefix.allEntries(prefix::class)
+            .map { unit -> prefix.rescale(unit) * magnitude to unit }
+            .lastOrNull { (magnitude, _) -> magnitude >= 1 } ?: (0.0 to prefix.base))
+            .let { (magnitude, prefix) -> format(magnitude, prefix.toString(), unit) }
+    }
+
     val readable by lazy {
-        when {
-            autoScale -> {
-                (Prefix.allEntries(prefix::class)
-                    .map { unit -> prefix.rescale(unit) * magnitude to unit }
-                    .lastOrNull { (magnitude, _) -> magnitude >= 1 } ?: (0.0 to prefix.base))
-                    .let { (magnitude, prefix) -> format(magnitude, prefix.toString(), unit) }
-            }
-
-            else -> {
-                format(magnitude, prefix.toString(), unit)
-            }
-        }
-
-
+        format(magnitude, prefix.toString(), unit, false)
     }
 
     val isZero: Boolean by lazy { magnitude == 0.0 }
 
-    override fun toString(): String = readable
+    override fun toString(): String = readableAutoScale
 
     fun rescale(other: T): Measure<T> {
         val conversion = prefix.rescale(other)
-        return Measure(magnitude * conversion, other, unit, false)
+        return Measure(magnitude * conversion, other, unit)
     }
 
-    private fun format(magnitude: Double, prefix: String, unit: String): String {
-        val magnitude =
-            if (decimalFormat) DecimalFormat("0.################").format(magnitude) else magnitude.toString()
-        return if (prefix != "BASE") "$magnitude ${prefix.lowercase()}$unit" else "$magnitude $unit"
+    private fun format(magnitude: Double, prefix: String, unit: String, scientificNotation: Boolean = true): String {
+        val magnitudeFormatted =
+            if (!scientificNotation) DecimalFormat(DECIMAL_FORMAT).format(magnitude) else magnitude.toString()
+        return if (prefix != BASE) "$magnitudeFormatted ${prefix.lowercase()}$unit" else "$magnitudeFormatted $unit"
     }
 
     companion object {
+        const val DECIMAL_FORMAT = "0.################"
+        const val BASE = "BASE"
+
         fun <T> create(magnitude: Double, prefix: T, unit: String): Measure<T>
                 where T : Enum<T>, T : Prefix {
             return Measure(magnitude, prefix, unit)
