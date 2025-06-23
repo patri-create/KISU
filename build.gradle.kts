94a95ed425707f3645f9af9c080cf8c83ec29987
Saul Diaz <sefford@gmail.com>
2025-06-23 23:18:03 +0200

    Enables Kotest Parallelism

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,13 @@ subprojects {
 
     tasks.withType<Test>().configureEach {
         useJUnitPlatform()
+
+        // Dynamically scale the parallelism of Kotest to the maximum cores supported by the hardware
+        val cores = Runtime.getRuntime().availableProcessors()
+        systemProperty("kotest.framework.parallelism", cores.toString())
+        systemProperty("kotest.framework.concurrent.specs", "true")
+        logger.info("Configured Kotest parallelism to $cores threads")
+
         finalizedBy("jacocoTestReport")
     }
 
