d47c8e7014e759978da9934a066f0863ad19a9ae
Saul Diaz <sefford@gmail.com>
2025-08-02 12:39:21 +0200

    Creates Unit Representation Class

--- lib/src/test/kotlin/org/kisu/test/generators/Units.kt
+++ lib/src/test/kotlin/org/kisu/test/generators/Units.kt
@@ -2,8 +2,17 @@ package org.kisu.test.generators
 
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.char
+import io.kotest.property.arbitrary.filter
+import io.kotest.property.arbitrary.flatMap
 import io.kotest.property.arbitrary.map
+import org.kisu.units.representation.Unit
 
-object Units : Generator<String> {
-    override val generator: Arb<String> = Arb.char('a'..'z').map { it.toString() }
+object Units : Generator<Unit> {
+    val symbols = Arb.char('a'..'z').map { it.toString() }
+
+    override val generator: Arb<Unit> = symbols.map { symbol -> Unit(symbol, 1) }
+
+    val distinct: Arb<Pair<Unit, Unit>> = generator.flatMap { left ->
+        generator.filter { right -> right != left }.map { right -> left to right }
+    }
 }
