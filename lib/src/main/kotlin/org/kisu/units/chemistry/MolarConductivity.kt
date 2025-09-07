93d5f51fc2e248b72bd08c04b8c290866de75233
Saul Diaz <sefford@gmail.com>
2025-09-07 11:57:59 +0200

    Moves unit description into the classes

--- lib/src/main/kotlin/org/kisu/units/chemistry/MolarConductivity.kt
+++ lib/src/main/kotlin/org/kisu/units/chemistry/MolarConductivity.kt
@@ -3,51 +3,12 @@ package org.kisu.units.chemistry
 import org.kisu.prefixes.Metric
 import org.kisu.units.Measure
 import org.kisu.units.base.Mole
+import org.kisu.units.chemistry.MolarConductivity.Companion.SiemensSquareMetrePerMole
 import org.kisu.units.representation.Product
 import org.kisu.units.representation.Quotient
 import org.kisu.units.special.Siemens
 import org.kisu.units.special.SquareMetre
 import java.math.BigDecimal
-
-/**
- * Represents the SI unit **siemens square metre per mole (S·m²/mol)**.
- *
- * This unit is used to measure **molar conductivity**, i.e., the electrical
- * conductivity of an electrolyte solution normalized by its molar concentration.
- * It is defined as the [Quotient] of the [Product] of [Siemens] (conductance) and
- * [SquareMetre] (area) divided by [Mole] (amount of substance).
- *
- * Example usages include:
- * - Determining ion mobility in electrolyte solutions
- * - Characterising strong and weak electrolytes
- * - Electrochemistry and materials science applications
- *
- * @see MolarConductivity for the physical quantity represented by this unit.
- */
-typealias SiemensSquareMetrePerMole = Quotient<Product<Siemens, SquareMetre>, Mole>
-
-/**
- * Creates a measure of **siemens square metres per mole** (S·m²/mol).
- *
- * This derived unit can be used in electrochemistry and related
- * fields to express conductivity–area products per amount of substance.
- *
- * Internally this returns a [Quotient] of:
- *  - a [Product] of [Siemens] (electrical conductance) with the specified [prefix]
- *    and [SquareMetre] (area)
- *  - divided by a [Mole] (amount of substance)
- *
- * @param prefix Metric prefix to apply to the siemens unit.
- * Defaults to [Metric.BASE] (no prefix).
- *
- * @return A [Quotient] representing S·m²/mol.
- */
-@Suppress("FunctionNaming")
-internal fun SiemensSquareMetrePerMole(prefix: Metric = Metric.BASE): Quotient<Product<Siemens, SquareMetre>, Mole> =
-    Quotient(
-        Product(Siemens(prefix), SquareMetre()),
-        Mole()
-    )
 
 /**
  * Represents the physical quantity of **molar conductivity**.
@@ -74,11 +35,59 @@ internal fun SiemensSquareMetrePerMole(prefix: Metric = Metric.BASE): Quotient<P
 class MolarConductivity(
     magnitude: BigDecimal,
     expression: SiemensSquareMetrePerMole
-) : Measure<SiemensSquareMetrePerMole, MolarConductivity>(magnitude, expression, ::MolarConductivity) {
+) : Measure<MolarConductivity.SiemensSquareMetrePerMole, MolarConductivity>(
+    magnitude = magnitude,
+    expression = expression,
+    create = ::MolarConductivity
+) {
 
     internal constructor(magnitude: BigDecimal, prefix: Metric = Metric.BASE) :
         this(
             magnitude,
             SiemensSquareMetrePerMole(prefix)
         )
+
+    /**
+     * Represents the SI unit **siemens square metre per mole (S·m²/mol)**.
+     *
+     * This unit is used to measure **molar conductivity**, i.e., the electrical
+     * conductivity of an electrolyte solution normalized by its molar concentration.
+     * It is defined as the [Quotient] of the [Product] of [Siemens] (conductance) and
+     * [SquareMetre] (area) divided by [Mole] (amount of substance).
+     *
+     * Example usages include:
+     * - Determining ion mobility in electrolyte solutions
+     * - Characterising strong and weak electrolytes
+     * - Electrochemistry and materials science applications
+     *
+     * @see MolarConductivity for the physical quantity represented by this unit.
+     */
+    typealias SiemensSquareMetrePerMole = Quotient<Product<Siemens, SquareMetre>, Mole>
+
+    companion object {
+        /**
+         * Creates a measure of **siemens square metres per mole** (S·m²/mol).
+         *
+         * This derived unit can be used in electrochemistry and related
+         * fields to express conductivity–area products per amount of substance.
+         *
+         * Internally this returns a [Quotient] of:
+         *  - a [Product] of [Siemens] (electrical conductance) with the specified [prefix]
+         *    and [SquareMetre] (area)
+         *  - divided by a [Mole] (amount of substance)
+         *
+         * @param prefix Metric prefix to apply to the siemens unit.
+         * Defaults to [Metric.BASE] (no prefix).
+         *
+         * @return A [Quotient] representing S·m²/mol.
+         */
+        @Suppress("FunctionNaming")
+        internal fun SiemensSquareMetrePerMole(
+            prefix: Metric = Metric.BASE
+        ): Quotient<Product<Siemens, SquareMetre>, Mole> =
+            Quotient(
+                Product(Siemens(prefix), SquareMetre()),
+                Mole()
+            )
+    }
 }
